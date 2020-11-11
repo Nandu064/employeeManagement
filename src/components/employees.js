86de6e022ca85326f11e21d1c93c0fc89a6edeab
Nandu064 <harish861432@gmail.com>
2020-11-11 20:01:18 +0530

    commited on 11-11-2020

---
+++ src/components/employees.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Employee from './employee'
+
+function employees() {
+    return (
+        <div>
+            <Employee />
+            <Employee />
+        </div>
+    )
+}
+
+export default employees
